73294dbfa5c90a15e571166a1efe3f0df4018ef8
oldviz <oldvis@ya.ru>
2022-07-17 22:12:13 +0400

    feat: completing task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  brackets = {};
+  stack = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] in brackets && str[i] !== brackets[str[i]]) {
+      stack.push(str[i])
+    }
+    else if (str[i] in brackets && str[i] === brackets[str[i]]) {
+      str[i] !== stack[stack.length - 1] ? stack.push(str[i]) : brackets[stack.pop()]   // <-----------
+    }
+    else if (brackets !== 0 && str[i] === brackets[stack.pop()]) {
+    }
+    else {
+      return false
+    }
+  }
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
